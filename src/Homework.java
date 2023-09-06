3752731167aef806608a625aae53c16277264cd7
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-09-06 10:31:16 -0600

    hw

--- src/Homework.java
+++ src/Homework.java
@@ -6,50 +6,58 @@ public class Homework {
 
 
 
-        int F0 =
-                72
+        int x=1
                 ;
 
-        int F1 = 1;
-
-        int F2 = F0+F1;
-
-        int F3 = F1+F2;
-
-        int F4 = F2+F3;
-
-        int F5 = F3+F4;
-
-        int F6 = F4+F5;
-
-        int F7 = F5+F6;
+        int y=0;
 
 
 
-        System.out.println(F7);
+        if(x==0){
+
+            y=0;
+
+        }
+
+        else if(x==1){
+
+            y=1;
+
+        }
+
+        else if(x==2){
+
+            y=4;
+
+        }
+
+        else if(x==3){
+
+            y=9;
+
+        }
+
+        else if(x==4){
+
+            y=17;
+
+        }
+
+        else if(x==5){
+
+            y=25;
+
+        }
+
+        else{
+
+            System.out.println("Error");
+
+        }
 
 
-        // In order to extract information we need to create a Scanner
 
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("What is your name?");
-
-        String name = input.nextLine();
-
-        System.out.println("What is your favorite food?");
-
-        String food = input.nextLine();
-
-        System.out.println("What is your age?");
-
-        int age = input.nextInt();
-
-        int year = 2023 - age;
-
-        String response = name+food+year;
-
-        System.out.println(response);
+        System.out.println("x="+x+", y="+y);
 
     }
 

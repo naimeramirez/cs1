27fb7b1d8af44bb25a606bd3cc9448f870730116
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-09-11 10:09:24 -0600

    concatenation

--- src/Concatenation.java
+++ src/Concatenation.java
@@ -3,16 +3,22 @@ public class Concatenation {
     public static void main(String[] args) {
 
         String month = "September";
-
         String day = "Monday";
-
         String major = "COMPUTER SCIENCE";
 
         String connected = "Today is " + month + ", 2023. The day is " + day + ". Our study major is " + major + ".";
-
         System.out.println(connected);
 
+        int length = ((month.concat(day)).concat(major)).length();
+        System.out.println(length);
 
+        String firstName = "Naime";
+        String lastName = "Ramirez";
+        System.out.println(firstName + " " + lastName);
+
+        String initials = firstName.charAt(0) + "." + lastName.charAt(0) + ".";
+
+        System.out.println(initials);
 
 
 

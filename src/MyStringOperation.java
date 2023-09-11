ad2306d412750aa3da86b7300a9cc9a42304aa6e
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-09-11 09:17:20 -0600

    string operations

---
+++ src/MyStringOperation.java
@@ -0,0 +1,21 @@
+public class MyStringOperation {
+
+    public static void main(String[] args) {
+
+        String word = "pRoGrAmiNg iS, FuN?";
+        System.out.println(word);
+
+        int wordLength = word.length();
+        System.out.println(wordLength);
+
+        String wordToLowerCase = word.toLowerCase();
+        System.out.println(wordToLowerCase);
+
+
+
+
+
+
+    }
+
+}

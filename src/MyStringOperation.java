3b2a259478dd023efcf538479aaf58cf450bc039
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-09-11 09:27:07 -0600

    string object

--- src/MyStringOperation.java
+++ src/MyStringOperation.java
@@ -11,10 +11,20 @@ public class MyStringOperation {
         String wordToLowerCase = word.toLowerCase();
         System.out.println(wordToLowerCase);
 
+        char wordFirstLetter = word.charAt(0);
+        System.out.println(wordFirstLetter);
 
+        char wordSecondLetter = word.charAt(1);
+        System.out.println(wordSecondLetter);
 
+        char wordLastCharacter = word.charAt(wordLength - 1);
+        System.out.println(wordLastCharacter);
 
+        String wordFun = (word.substring(15, 18)).toUpperCase();
+        System.out.println(wordFun);
 
+        String wordPrograming = (word.substring(0, 10)).toLowerCase();
+        System.out.println(wordPrograming);
 
     }
 

ea0af94a7a5b8f6360d343a8bc651a9d59a74b61
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-09-12 12:44:14 -0600

    hw10

--- src/Homework.java
+++ src/Homework.java
@@ -2,100 +2,30 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Homework {
-
     public static void main(String[] args) {
 
-        Random random = new Random();
+                String question = "how are you today?";
+                String answer = "I do not know";
+
+                switch (question){
+                    case "2+2":
+                        answer = "4";
+                        break;
+                    case "2+3":
+                        answer = "5";
+                        break;
+                    case "hello":
+                        answer = "hello";
+                        break;
+                    default:
+                        answer = "I do not know";
+                }
+
+                System.out.println("question="+question);
+                System.out.println("answer="+answer);
 
 
 
-        int c1 = 0;
-
-        int c2 = 2;
-
-        int c3 = 1;
-
-        int c4 = -2;
-
-
-
-        System.out.println("c1= "+c1);
-
-        System.out.println("c2= "+c2);
-
-        System.out.println("c3= "+c3);
-
-        System.out.println("c4= "+c4);
-
-
-
-        int x = 2;
-
-        int y = 2;
-
-
-
-        if (c1>0){
-
-            x = x+1;
-
-        }
-
-        else if(c1<0){
-
-            x = x-1;
-
-        }
-
-
-
-        if (c2>0){
-
-            y = y+1;
-
-        }
-
-        else if (c2<0){
-
-            y = y-1;
-
-        }
-
-
-
-        if (c3>0){
-
-            x = x+1;
-
-        }
-
-        else if(c3<0){
-
-            x = x-1;
-
-        }
-
-
-
-        if (c4>0){
-
-            y = y+1;
-
-        }
-
-        else if(c4<0){
-
-            y = y-1;
-
-        }
-
-
-
-        System.out.println("x= "+x);
-
-        System.out.println("y= "+y);
 
     }
-
-
 }

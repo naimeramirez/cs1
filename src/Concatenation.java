4b704c46bd9054114b84d27090949e33dc9ebad6
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-09-13 10:03:35 -0600

    concatenation

--- src/Concatenation.java
+++ src/Concatenation.java
@@ -3,7 +3,7 @@ public class Concatenation {
     public static void main(String[] args) {
 
         String month = "September";
-        String day = "Monday";
+        String day = "Wednesday";
         String major = "COMPUTER SCIENCE";
 
         String connected = "Today is " + month + ", 2023. The day is " + day + ". Our study major is " + major + ".";

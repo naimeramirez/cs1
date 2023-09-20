2827453ae330ab56de388d9d39be8a73eb65cc55
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-09-20 10:03:59 -0600

    assignment 3

--- src/OrderingAndStringComparison.java
+++ src/OrderingAndStringComparison.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+
 public class OrderingAndStringComparison {
     public static void main(String[] args) {
         Random random = new Random();
@@ -15,23 +16,83 @@ public class OrderingAndStringComparison {
         }
 
 
-        String str1 = "a";
-        String str2 = "b";
+        String str1example = "a";
+        String str2example = "b";
 
-        System.out.println(str1.compareTo(str2)); //-1
+        System.out.println(str1example.compareTo(str2)); //-1
 
-        if (str1.compareTo(str2) < 0) {
-            System.out.println(str1);
-            System.out.println(str2);
+        if (str1example.compareTo(str2example) < 0) {
+            System.out.println(str1example);
+            System.out.println(str2example);
         } else {
-            System.out.println(str2);
-            System.out.println(str1);
+            System.out.println(str2example);
+            System.out.println(str1example);
         }
 
-        if (str1.equals(str2)) {
+        if (str1example.equals(str2example)) {
             System.out.println("Strings are the same");
         } else {
             System.out.println("Strings are different");
+        }
+
+// -------- chapter 2 assignment 3 --------
+
+        String str1 = "c";
+        String str2 = "a";
+        String str3 = "b";
+
+        int value1 = str1.compareTo(str2);
+        System.out.println(value1); //-1
+
+        int value2 = str2.compareTo(str1);
+        System.out.println(value2); //1
+
+        int value3 = str1.compareTo(str1);
+        System.out.println(value3); //0
+
+        // two strings
+        if(str1.compareTo(str2) < 0){
+            //System.out.println("First string: "+str1);
+            //System.out.println("Second string: "+str2);
+
+            if(str1.compareTo(str3) < 0){
+                if(str2.compareTo(str3) < 0){
+                    System.out.println("First string: "+str1);
+                    System.out.println("Second string: "+str2);
+                    System.out.println("Third string: "+str3);
+                }else{
+                    System.out.println("First string: "+str1);
+                    System.out.println("Second string: "+str3);
+                    System.out.println("Third string: "+str2);
+                }
+            }
+            else{
+                System.out.println("First string: "+str3);
+                System.out.println("Second string: "+str1);
+                System.out.println("Third string: "+str2);
+            }
+        }
+        else{
+            //System.out.println("First string: "+str2);
+            //System.out.println("Second string: "+str1);
+
+            if(str2.compareTo(str3) < 0){
+                if(str1.compareTo(str3) < 0){
+                    System.out.println("First string: "+str2);
+                    System.out.println("Second string: "+str1);
+                    System.out.println("Third string: "+str3);
+                }
+                else{
+                    System.out.println("First string: "+str2);
+                    System.out.println("Second string: "+str3);
+                    System.out.println("Third string: "+str1);
+                }
+            }
+            else{
+                System.out.println("First string: "+str3);
+                System.out.println("Second string: "+str2);
+                System.out.println("Third string: "+str1);
+            }
         }
 
     }

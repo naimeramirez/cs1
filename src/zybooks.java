2841078e6a396be83cd31c6ad3cb7eaac624676a
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-09-25 09:36:40 -0600

    2.10.1

--- src/zybooks.java
+++ src/zybooks.java
@@ -6,19 +6,13 @@ public class zybooks {
 //        String str = scnr.nextLine();
 //        int num = scnr.nextInt();
 
-        int shipWeightPounds;
-        int shipCostCents = 0;
-        final int FLAT_FEE_CENTS = 75;
+        double initMass = 10.0;
+        double growthRate = 1.0;
+        double yearsGrow = 3;
+        double finalMass;
+        finalMass = initMass * Math.pow(1.0 + growthRate, yearsGrow);
 
-        final int CENTS_PER_POUND = 25;
-
-        shipWeightPounds = scnr.nextInt();
-        shipCostCents = (shipWeightPounds * CENTS_PER_POUND) + FLAT_FEE_CENTS;
-
-        System.out.println("Weight(lb): " + shipWeightPounds);
-        System.out.println("Flat fee(cents): " + FLAT_FEE_CENTS);
-        System.out.println("Cents per pound: " + CENTS_PER_POUND);
-        System.out.println("Shipping cost(cents): " + shipCostCents);
+        System.out.println(finalMass);
 
     }
 }

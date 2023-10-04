d5a0dd4b40f3cfd875bd066166909de1a7c2df9b
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-10-04 12:26:19 -0600

    hw

---
+++ src/Numbers.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Numbers {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        int num1 = scnr.nextInt();
+        int num2 = scnr.nextInt();
+        int num3 = scnr.nextInt();
+
+        if (((num1 < 10) || (num1 > 50)) || ((num2 < 10) || (num2 > 50)) || ((num3 < 10) || (num3 > 50))) {
+            System.out.println("invalid number");
+        } else if ((num1 == num2) || (num1 == num3) || (num2 == num3)) {
+            System.out.println("equal numbers");
+        } else {
+            if ((num1 < num2) && (num1 < num3)) {
+                if (num2 < num3) {
+                    System.out.println(num1 + " " + num2 + " " + num3);
+                } else {
+                    System.out.println(num1 + " " + num3 + " " + num2);
+                }
+            } else if ((num2 < num1) && (num2 < num3)) {
+                if ((num1 < num2) && (num1 < num3)) {
+                    if (num2 < num3) {
+                        System.out.println(num1 + " " + num2 + " " + num3);
+                    } else {
+                        System.out.println(num1 + " " + num3 + " " + num2);
+                    }
+
+                } else if ((num3 < num1) && (num3 < num2)) {
+                    if ((num1 < num2) && (num1 < num3)) {
+                        if (num2 < num3) {
+                            System.out.println(num1 + " " + num2 + " " + num3);
+                        } else {
+                            System.out.println(num1 + " " + num3 + " " + num2);
+                        }
+                    }
+                }
+
+                // Write a program that asks the computer to enter three values.
+                //Each of these values must integers between 10 and 30
+                //The program shall find out the smallest and the largest of the three
+                //Display them using System.out.println():. If the three values are the same,
+                //the program shall display "Multiple values identical".
+            }
+        }
+    }
+}
--- src/zybooks.java
+++ src/zybooks.java
@@ -35,7 +35,3 @@ public class zybooks {
 
     }
 }
-
-//     and service the primary highway indicated by the rightmost two digits. Thus, I-405 services I-5, and I-290 services I-90.
-
-//        Given a highway number, indicate whether it is a primary or auxiliary highway. If auxiliary, indicate what primary highway it serves. Also indicate if the (primary) highway runs north/south or east/west.

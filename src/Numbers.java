e1d737aca1b27e3cf78bc899acd9c3656accdc9d
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-10-04 12:33:29 -0600

    hw

--- src/Numbers.java
+++ src/Numbers.java
@@ -10,39 +10,42 @@ public class Numbers {
 
         if (((num1 < 10) || (num1 > 50)) || ((num2 < 10) || (num2 > 50)) || ((num3 < 10) || (num3 > 50))) {
             System.out.println("invalid number");
-        } else if ((num1 == num2) || (num1 == num3) || (num2 == num3)) {
-            System.out.println("equal numbers");
+        } else if ((num1 == num2) && (num2 == num3)) {
+            System.out.println("Multiple values identical");
         } else {
             if ((num1 < num2) && (num1 < num3)) {
                 if (num2 < num3) {
-                    System.out.println(num1 + " " + num2 + " " + num3);
+                    System.out.println("Smallest value: " + num1);
+                    System.out.println("Largest value: " + num3);
                 } else {
-                    System.out.println(num1 + " " + num3 + " " + num2);
+                    System.out.println("Smallest value: " + num1);
+                    System.out.println("Largest value: " + num2);
                 }
             } else if ((num2 < num1) && (num2 < num3)) {
-                if ((num1 < num2) && (num1 < num3)) {
-                    if (num2 < num3) {
-                        System.out.println(num1 + " " + num2 + " " + num3);
-                    } else {
-                        System.out.println(num1 + " " + num3 + " " + num2);
-                    }
-
-                } else if ((num3 < num1) && (num3 < num2)) {
-                    if ((num1 < num2) && (num1 < num3)) {
-                        if (num2 < num3) {
-                            System.out.println(num1 + " " + num2 + " " + num3);
-                        } else {
-                            System.out.println(num1 + " " + num3 + " " + num2);
-                        }
-                    }
+                if (num1 < num3) {
+                    System.out.println("Smallest value: " + num2);
+                    System.out.println("Largest value: " + num3);
+                } else {
+                    System.out.println("Smallest value: " + num2);
+                    System.out.println("Largest value: " + num1);
                 }
 
-                // Write a program that asks the computer to enter three values.
-                //Each of these values must integers between 10 and 30
-                //The program shall find out the smallest and the largest of the three
-                //Display them using System.out.println():. If the three values are the same,
-                //the program shall display "Multiple values identical".
+            } else if ((num3 < num1) && (num3 < num2)) {
+                if (num2 < num1) {
+                    System.out.println("Smallest value: " + num3);
+                    System.out.println("Largest value: " + num1);
+                } else {
+                    System.out.println("Smallest value: " + num3);
+                    System.out.println("Largest value: " + num2);
+                }
             }
+
+            // Write a program that asks the computer to enter three values.
+            //Each of these values must be integers between 10 and 30
+            //The program shall find out the smallest and the largest of the three
+            //Display them using System.out.println():. If the three values are the same,
+            //the program shall display "Multiple values identical".
+
         }
     }
 }

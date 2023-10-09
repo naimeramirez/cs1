0b8498655c26369d1dba76e4e642903457dff9ca
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-10-09 10:30:48 -0600

    lab 4

--- src/CreatePassword.java
+++ src/CreatePassword.java
@@ -4,5 +4,21 @@ public class CreatePassword {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
 
+        System.out.println("First Name: ");
+        String firstName = scnr.nextLine();
+
+        System.out.println("Last Name: ");
+        String lastName = scnr.nextLine();
+
+        System.out.println("Four Digits: ");
+        String fourDigits = scnr.nextLine();
+
+        String Password;
+
+        String first6LettersOfFirstName;
+        String firstLetterOfLastName;
+
+//        Output the login name, which is made up of the first six letters of the first name, followed by the first letter of the last name, an underscore (_), and then the last digit of the number (use the % operator). If the first name has less than six letters, then use all letters of the first name.
+
     }
 }

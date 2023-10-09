f8ca84ba0efaa3d4a20fcdfb9fefe0fe93b17823
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-10-09 10:24:26 -0600

    lab 4

--- src/CreatePassword.java
+++ src/CreatePassword.java
@@ -1,12 +1,8 @@
 import java.util.Scanner;
 
-public class zybooks {
+public class CreatePassword {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-//        String str = scnr.nextLine();
-//        int num = scnr.nextInt();
-
-
 
     }
 }

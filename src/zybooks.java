4283d079324e1c19740c0dab1f078623b7b9c490
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-10-16 10:35:04 -0600

    rock paper scissors

--- src/zybooks.java
+++ src/zybooks.java
@@ -6,7 +6,26 @@ public class zybooks {
 //        String str = scnr.nextLine();
 //        int num = scnr.nextInt();
 
+        int nextChoice;
 
+        nextChoice = scnr.nextInt();
+
+        switch (nextChoice) {
+            case 0:
+                System.out.println("Rock");
+                break;
+            case 1:
+                System.out.println("Paper");
+                break;
+            case 2:
+                System.out.println("Scissors");
+                break;
+            default:
+                System.out.println("Unknown");
+                break;
+        }
 
     }
 }
+
+//    Write a switch statement that checks nextChoice. If 0, print "Rock". If 1, print "Paper". If 2, print "Scissors". For any other value, print "Unknown". End with newline.

d63529d2f87bf0293054b279157f0fc0d56b9e3e
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-10-16 09:42:48 -0600

    how

--- src/CreatePassword.java
+++ src/CreatePassword.java
@@ -4,19 +4,26 @@ public class CreatePassword {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
 
-        System.out.println("First Name: ");
-        String firstName = scnr.nextLine();
+        String firstName = scnr.next();
 
-        System.out.println("Last Name: ");
-        String lastName = scnr.nextLine();
+        String lastName = scnr.next();
 
-        System.out.println("Four Digits: ");
-        String fourDigits = scnr.nextLine();
-
-        String Password;
+        String fourDigits = scnr.next();
 
         String first6LettersOfFirstName;
-        String firstLetterOfLastName;
+
+        if (firstName.length() >= 6) {
+            first6LettersOfFirstName = firstName.substring(0, 5);
+        } else {
+            first6LettersOfFirstName = firstName;
+        }
+
+        String firstLetterOfLastName = String.valueOf(lastName.charAt(0)).toUpperCase();
+        String lastDigit = String.valueOf(fourDigits.charAt(fourDigits.length() - 1));
+
+        String password = first6LettersOfFirstName + firstLetterOfLastName + "_" + lastDigit;
+
+        System.out.println(password);
 
 //        Output the login name, which is made up of the first six letters of the first name, followed by the first letter of the last name, an underscore (_), and then the last digit of the number (use the % operator). If the first name has less than six letters, then use all letters of the first name.
 

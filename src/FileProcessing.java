70193d403483e287d8e510d532fad38f089db7da
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-10-25 12:55:38 -0600

    file processing

--- src/FileProcessing.java
+++ src/FileProcessing.java
@@ -9,17 +9,45 @@ public class FileProcessing {
 
         double sum = 0;
         double numOfElements = 0;
+        double mean = 0;
+
+        double sub = 0;
+        double pow = 0;
+        double sigma = 0;
+        double frac = 0;
+        double stdev = 0;
+
+        double smallest = 0;
+        double largest = 0;
+
 
 
         while(fileScanner.hasNext()) {
             double currentNum = fileScanner.nextInt();
             sum += currentNum;
             numOfElements++;
+            mean = sum/numOfElements;
+
+            sub = currentNum - mean;
+            pow = Math.pow(sub, 2);
+            sigma = pow + numOfElements;
+            frac = sigma / (numOfElements - 1);
+            stdev = Math.sqrt(frac);
+
+            if (numOfElements == 1 || currentNum < smallest) {
+                smallest = currentNum;
+            }
+
+            if (numOfElements == 1 || currentNum > largest) {
+                largest = currentNum;
+            }
 //            String current = fileScanner.nextLine();
 //            System.out.println(current);
         }
-        System.out.println("Mean:");
-        System.out.println(sum/numOfElements);
+        System.out.println("Mean: " + mean);
+        System.out.println("Standard Deviation: " + stdev);
+        System.out.println("Smallest: " + smallest);
+        System.out.println("Largest: " + largest);
 
 
     }

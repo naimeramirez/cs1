f23a3d7ca76134531c5acf3a838b309a86c069e0
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-10-25 09:22:50 -0600

    hw

--- src/zybooks.java
+++ src/zybooks.java
@@ -6,20 +6,26 @@ public class zybooks {
 //        String str = scnr.nextLine();
 //        int num = scnr.nextInt();
 
-        char charInput;
-        int sumChars;
+        int result;
+        int stop;
+        int a;
+        int b;
 
-        sumChars = 0;
-        charInput = scnr.next().charAt(0);
+        stop = scnr.nextInt();
+        result = 0;
 
-        while (charInput != 'e') {
-            sumChars++;
-            charInput = scnr.next().charAt(0);
+        for (a = 0; a < 3; ++a) {
+            System.out.print(a + ": ");
+            for (b = 0; b < 2; ++b) {
+                result += a + b;
+                if (result > stop) {
+                    System.out.print("_ ");
+                    continue;
+                }
+                System.out.print(result + ",");
+            }
+            System.out.println();
         }
-
-        System.out.println(sumChars);
 
     }
 }
-
-//    Character charInput is read from input. Write a while loop that reads characters from input until character 'e' is read. Then, count the total number of characters read. Character 'e' should not be included in the count.

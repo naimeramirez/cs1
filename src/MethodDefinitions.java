5e09025ad9d99a6f1036fdaee4819c5669f2f07d
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-11-01 12:48:32 -0600

    method definitions

---
+++ src/MethodDefinitions.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class MethodDefinitions {
+    public static void main(String[] args) {
+    }
+
+// 1.   public static int calculateSum (int x, int y, int z) { }
+
+// 2.   public static String getInitials (String s1, String s2) { }
+
+// 3.   public static void printAll (String s) { }
+
+// 4.   public static boolean isPositive (int n) { }
+
+// 5.   public static void printReverse (String s) { }
+
+// 6.   public static int printAge (int year) { }
+
+// 7.   public static void printEvenNumbers (int n) { }
+
+// 8.   public static void printOddNumbers (int n) { }
+
+// 9.   public static void printEvensByRange (int x, int y) { }
+
+// 10.   public static void printFibonacciNumbers (int n) { }
+
+// 11.   public static double getAVG (double x, double y, double z) { }
+
+    public static void readNumbers (FileProcessing) { }
+
+// 13.   public static char getThird (String s) { }
+
+// 14.   private static int setX (double n) { }
+
+// 15.   private static Object getTime () { }
+
+// 16.   protected static void isUpperCase (char n) { }
+
+// 17.   protected static long currentTimeMillis () { }
+
+// 18.   protected static long nanoTime () { }
+
+// 19.   public static double random () { }
+
+// 20.   public void int processArgs(ArrayList[] args) { }
+
+}

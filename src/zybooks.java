c320f281cc7d5ee46daef5088ad688323624b876
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-11-01 10:32:25 -0600

    hw

--- src/zybooks.java
+++ src/zybooks.java
@@ -8,28 +8,6 @@ public class zybooks {
 //        String str = scnr.next();
 //        int num = scnr.nextInt();
 
-        Scanner scnr = new Scanner(System.in);
-        String userString;
-        // Add more variables as needed
-
-        userString = scnr.next();
-        /* Type your code here. */
-
-//        Character.isDigit()
-        boolean isInteger = true;
-
-        for (int i = 0; i < userString.length(); i++) {
-            if (!Character.isDigit(userString.charAt(i))) {
-                isInteger = false;
-                break;
-            }
-        }
-
-        if (isInteger) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
 
     }
 }

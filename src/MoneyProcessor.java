f1a4dd4df7693a17ec4f1a33a57d8dd6c276abfe
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-12-06 12:53:10 -0700

    money processor

--- src/MoneyProcessor.java
+++ src/MoneyProcessor.java
@@ -7,20 +7,28 @@ public class MoneyProcessor {
     public static void main(String[]args) throws IOException {
         Scanner fileScanner = new Scanner(new File("/Users/ramirez/IdeaProjects/cs1/src/money.txt"));
 
+        Dollar[] wallet = new Dollar[2];
+        int index = 0;
+
         while(fileScanner.hasNext()) {
             String line = fileScanner.nextLine();
-//            System.out.println(line);
 
             String[] lineArray = line.split(",");
             Dollar dollar = new Dollar(Integer.parseInt(lineArray[0]), lineArray[1], lineArray[2], Integer.parseInt(lineArray[3]));
 
-            for (int i = 0; i < lineArray.length; ++i) {
-//                System.out.println(lineArray[i]);
-            }
-
             dollar.showBill();
 
+            if (index < wallet.length) {
+                wallet[index] = dollar;
+                ++index;
+            } else {
+                break;
+            }
+
         }
+
+        System.out.println("Wallet 0: " + wallet[0].getDenomination());
+        System.out.println("Wallet 0: " + wallet[1].getDenomination());
 
     }
 }
